5ff59d9ccd8aa7e1805342bcc283eae4046edbd6
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-12-15 11:05:36 -0600

    Creacion clase de Presentacion (Base)

--- src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,15 +1,33 @@
 package gm.zona_fit;
 
+import gm.zona_fit.servicio.IClienteServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZonaFitApplication {
+public class ZonaFitApplication implements CommandLineRunner {
+
+	@Autowired //Inyectamos dependencias
+	private IClienteServicio clienteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Iniciando la aplicacion");
+		//Levantar la fabrica de Spring
 		SpringApplication.run(ZonaFitApplication.class, args);
 
-		System.out.println("Hola mundo");
+
+		logger.info("Aplicacion Finalizada");
 	}
 
+	@Override
+	public void run(String... args) throws Exception { //Este metodo solo se ejecuta si esta en funcionamiento la fabrica de Spring
+		//Ya no se trabaja sobre la main
+		logger.info("*** Aplicacion Zona Fit (GYM) ***");
+	}
 }
