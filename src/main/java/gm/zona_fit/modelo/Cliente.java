1af967f805784f825d867b7c8ce571c247e6c5c5
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-12-15 10:21:36 -0600

    Configuracion de la clase identidad (Cliente)

--- src/main/java/gm/zona_fit/modelo/Cliente.java
+++ src/main/java/gm/zona_fit/modelo/Cliente.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Data //Con esto generamos los metodos Get y Set de cada atributo de nuestra entidad/Clase
+@NoArgsConstructor //Crea un constructor vacio en nuestra clase
+@AllArgsConstructor //Crea un constructor con todos los argumentos
+@ToString //Agrega el metodo ToString a nuestra clase
+@EqualsAndHashCode //Agrega los metodos Equals y Hashcode
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Significa que la base de datos es responsable de generar un nuevo valor único para el ID de cada nuevo registro que se inserte en la tabla. Esto típicamente se hace con un campo auto-incremento.
